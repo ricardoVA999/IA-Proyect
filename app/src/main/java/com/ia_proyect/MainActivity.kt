76b2bf6967cfbbe26442bc6fe6a0e56575975e62
ricardoVA999 <40800457+ricardova999@users.noreply.github.com>
2021-05-25 14:58:54 -0600

    Comments

--- app/src/main/java/com/ia_proyect/MainActivity.kt
+++ app/src/main/java/com/ia_proyect/MainActivity.kt
@@ -17,9 +17,14 @@ import com.theartofdev.edmodo.cropper.CropImage
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.label.Category
 
+// Inteligencia Artificial
+// Ricardo Antonio Valenzuela Avila
+// Maria Jose Castro
 
+// Funciones para la pantalla principal de la aplicacion
 class MainActivity : AppCompatActivity() {
 
+    // Definicion de los componentes de la pantalla
     lateinit var selectButton: Button
     lateinit var predButton: Button
     lateinit var camaraButton: Button
@@ -31,15 +36,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Asignacion a las variables con los componentes
         selectButton = findViewById(R.id.button)
         predButton = findViewById(R.id.button2)
         imgView = findViewById(R.id.imageView)
         text = findViewById(R.id.textView)
         camaraButton = findViewById(R.id.button3)
 
-        val labels = application.assets.open("dict.txt").bufferedReader().use { it.readText() }.split("\n")
-        print(labels)
-
+        // On click listener para el boton de selecionar, utiliza un intent para abrir la galeria
         selectButton.setOnClickListener(View.OnClickListener {
             Log.d("mssg", "button pressed")
             var intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
@@ -48,26 +52,26 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent, 100)
         })
 
+        // On Click Listener para el boton de preddicion, se encarga de hacer el resizing de la imagen y realizar la prediccion
         predButton.setOnClickListener(View.OnClickListener {
             var resized = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
             val model = JapaneseFoodModel.newInstance(this)
 
-// Creates inputs for reference.
+            // Creates inputs for reference.
             val image = TensorImage.fromBitmap(resized)
 
-// Runs model inference and gets result.
+            // Corriendo el modelo y obteniendo el resultado
             val outputs = model.process(image)
             val scores = outputs.scoresAsCategoryList
-            Log.d("myTag", scores.toString())
+            Log.d("Scores", scores.toString())
 
             val pred = getPred(scores)
 
             text.setText(pred)
-
-// Releases model resources if no longer used.
             model.close()
         })
 
+        // On Click listener para abrir la camara, esto se hace con dependencias de la comunidad
         camaraButton.setOnClickListener(View.OnClickListener {
             CropImage.activity().start(this)
         })
@@ -75,6 +79,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // Maneja lo relacionado a abrir galeria y camara
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
@@ -91,8 +96,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-
+    // Funcion que obtiene la categoria con el score mas alto
     fun getPred(scores: MutableList<Category>):String{
 
         var pred = ""
